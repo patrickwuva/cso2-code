c553be24addc54d86c1124f017cb2a2985f5ced2
Patrick Williamson <upm8pb@virginia.edu>
2023-12-14 02:26:58 -0500

    starting hw 1

---
+++ hw1/split.c
@@ -0,0 +1,12 @@
+char **string_split(const char *input, const char *sep, int *num_words){
+    
+    char **arr = (char **)malloc(2 * sizeof(char *));
+    
+    for(int i = 0; i < 2; i++){
+        arr[i] (char *)malloc(2 * sizeof(char));
+        arr[i] = " ";
+    }
+
+        sdqa    
+
+}
