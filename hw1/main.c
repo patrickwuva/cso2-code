433a00aa85ba83e2a048df53d91057582e1fdb52
Patrick Williamson <upm8pb@virginia.edu>
2024-01-19 22:42:53 -0500

    pushing to main

--- hw1/main.c
+++ hw1/main.c
@@ -1,10 +1,27 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "split.c"
 
 
-int main(){
-    int words = 0;
-    char** test = string_split("hello there","test",&words);
-    return 0;
+int main(int argc, char *argv[]){
+    char* sep = (char *)malloc(500 * sizeof(char));
+    
+    if(argc == 1){
+        sep = " \t";
+        sep = (char *)realloc(sep,sizeof(char)*strlen(sep));
+    }
+
+    else{
+        for(int i = 1; i < argc; i++){
+            strcat(sep, argv[i]);
+        }
+        sep =(char *)realloc(sep,sizeof(char)*strlen(sep));
+    }
+    
+    char input[4000];
+    while(fgets(input, 4000, stdin) != NULL){ // need to change this to exit if .
+        // call the split.c with input;
+        memset(input, 0, sizeof(input));
+    }
 }
