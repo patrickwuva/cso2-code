f7f7adf5a23c7f4ed4353ec8bbbfaabcebc548a0
Patrick Williamson <upm8pb@virginia.edu>
2024-02-01 00:42:54 -0500

    adding

--- hw1/split.c
+++ hw1/split.c
@@ -4,7 +4,6 @@
 
 char **string_split(const char *input, const char *sep, int *num_words){
   
-    printf("sep: %s size: %zu\n",sep,strlen(sep));
     int last_i = 0;
 
     char** slist = (char**)calloc(1, sizeof(char*));
@@ -13,20 +12,24 @@ char **string_split(const char *input, const char *sep, int *num_words){
         char tmp[2];
         tmp[0] = input[i];
         tmp[1] = '\0';
+        
+        //printf("char: %s strcspn: %zu\n",tmp,strcspn(sep,tmp));
+
         if(strcspn(sep,tmp) != (int)strlen(sep) || i == (int)strlen(input) - 1){
             if(*num_words !=0){
                 slist = realloc(slist, (1+*num_words) * sizeof(char*));
-            }
-   //need to double check how it is proccesing this not doing multiple seps in a row properly 
+            } 
+             
             strncpy(buffer,input+(last_i),i-last_i);
             buffer[i-last_i] = '\0';
            
             slist[*num_words] = (char*)malloc(strlen(buffer) * sizeof(char));
             strncpy(slist[*num_words],buffer,strlen(buffer));
             *num_words += 1;
+            memset(buffer,'\0',sizeof(buffer));
+            }
             last_i = i+1;
             
-            memset(buffer,'\0',sizeof(buffer));
         }
         else if(strcspn(sep,tmp) < strlen(sep)-1 && (i == 0 || i == (int)strlen(input) - 1)){
             
