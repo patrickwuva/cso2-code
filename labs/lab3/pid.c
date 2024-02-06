36654aa98f9995ed9a3daabb004e5e85c4134f23
Patrick Williamson <upm8pb@virginia.edu>
2024-02-06 16:10:54 -0500

    updating

--- labs/lab3/pid.c
+++ labs/lab3/pid.c
@@ -1,4 +1,4 @@
-#define _XOPEN_SOURCE 600 // request all POSIX features, even with -std=c11
+#define _XOPEN_SOURCE 700 // request all POSIX features, even with -std=c11
 #include <stdlib.h>       // for EXIT_SUCCESS, NULL, abort
 #include <stdio.h>        // for getline
 #include <unistd.h>       // for getpid
@@ -7,19 +7,28 @@
 #include <fcntl.h>
 #include <string.h>
 
-pid_t other_pid = 0;
 char *inbox_data;
 char *outbox_data;
+char inbox_filename[512];
+char outbox_filename[512];
 int size = 4096;
+pid_t other_pid = 0;
+static void cleanup(){
+    shm_unlink(inbox_filename);
+    shm_unlink(outbox_filename);
+    munmap(inbox_data, size);
+    munmap(outbox_data, size);
+}
 static void sigging(int signum){
 
     char msg_int[] = "sigusr1 reciered \n";
     if (signum == SIGTERM){
-        printf("sigterm received\n");
+        cleanup();
+        _exit(EXIT_SUCCESS);
     }
     if (signum == SIGINT){
-        //cleanup()
-        printf("sigint\n");
+        cleanup();
+        kill(other_pid,SIGTERM);
         _exit(EXIT_SUCCESS);
     }
     if (signum == SIGUSR1){
@@ -53,11 +62,7 @@ int main(void) {
     } while ((other_pid = strtol(line, NULL, 10)) == 0);
     free(line);
 
-
-    char inbox_filename[512];
     snprintf(inbox_filename, sizeof inbox_filename, "/%d-chat",getpid());
-    
-    char outbox_filename[512];
     snprintf(outbox_filename, sizeof outbox_filename, "/%d-chat",other_pid);
 
     int inbox_fd = shm_open(inbox_filename, O_CREAT | O_RDWR, 0666);
@@ -68,10 +73,6 @@ int main(void) {
     
     ftruncate(inbox_fd, size);
     ftruncate(outbox_fd, size);
-
-    //char *inbox_data;
-    //char *outbox_data;
-
 
     inbox_data = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, inbox_fd, 0);
     outbox_data = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, outbox_fd, 0);
@@ -90,12 +91,14 @@ int main(void) {
     char buffer[4096];
     while(1){
         if(fgets(buffer, sizeof(buffer), stdin) == NULL){
+            kill(other_pid, SIGTERM);
             break;
         }
         memcpy(outbox_data, buffer, strlen(buffer));
         kill(other_pid, SIGUSR1);
         while(outbox_data[0]) { usleep(10000); }
-    }   
+    }
 
+    cleanup();
     return EXIT_SUCCESS;
 }
