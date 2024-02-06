7f23ae0895fd0136854263f90eb50f3e26c70fb6
Patrick Williamson <upm8pb@virginia.edu>
2024-02-06 11:43:45 -0500

    updating

--- labs/lab3/pid.c
+++ labs/lab3/pid.c
@@ -2,10 +2,30 @@
 #include <stdlib.h>       // for EXIT_SUCCESS, NULL, abort
 #include <stdio.h>        // for getline
 #include <unistd.h>       // for getpid
+#include <signal.h>
 
 pid_t other_pid = 0;
 
+static void sigging(int signum){
+
+    if (signum == SIGTERM){
+        printf("sigterm received\n");
+    }:
+    )
+    if (signum == SIGINT){
+        printf("sigint received\n");
+    }
+    if (signum == SIGUSR1){
+        printf("sigusr1 received\n");
+    }
+}
+
 int main(void) {
+    struct sigaction sa;
+    sa.sa_handler = sigging;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+
     printf("This process's ID: %ld\n", (long) getpid());
     char *line = NULL; size_t line_length = 0;
     do {
