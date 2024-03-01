b2154c226bc60155a6223cb0ec8eef23544d51f5
Patrick Williamson <upm8pb@virginia.edu>
2024-02-29 19:25:44 -0500

    updating hw

---
+++ homework/hw4/config.h
@@ -0,0 +1,4 @@
+#include <stdint.h>
+#define LEVELS 2
+#define POBITS 12
+typedef uint64_t pageEntry;
---
+++ homework/hw4/mypagerisbeeping.c
@@ -0,0 +1,10 @@
+// time to cry
+#include "config.h"
+#include "mlpt.h"
+#include <stdint.h>
+
+
+size_t ptbr = 0;
+void page_allocate(size_t va){
+    
+}
