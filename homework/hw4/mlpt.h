af659e7f4691bb4ebbc007d36df2c300b55fc86c
Patrick Williamson <upm8pb@virginia.edu>
2024-03-01 14:08:21 -0500

    updating

--- homework/hw4/mlpt.h
+++ homework/hw4/mlpt.h
@@ -20,4 +20,6 @@ size_t translate(size_t va);
  * address. If there already is such a page, does nothing.
  */
 void page_allocate(size_t va);
+
+
 #endif
--- homework/hw4/mypagerisbeeping.c
+++ homework/hw4/mypagerisbeeping.c
@@ -1,10 +1,37 @@
 // time to cry
-#include "config.h"
-#include "mlpt.h"
+#define _XOPEN_SOURCE 700
+#include <stdio.h>
+#include <stdlib.h>
 #include <stdint.h>
+#include <assert.h>
+#include <string.h>
+#include "mlpt.h"
+#include "config.h"
+#include <math.h>
 
+size_t size = 4096;
+size_t alignment = 8;
 
 size_t ptbr = 0;
-void page_allocate(size_t va){
+
+
+void map(va){}
+void page_allocate(size_t va){        
+    void *ptr = NULL;
+    posix_memalign(&ptr, alignment, size);
+    ptbr = (size_t)ptr;
+    memset((void*)ptbr, 0, size);
+}
+
+int main(){
+    assert(ptbr == 0); 
+    page_allocate(0);
+    printf("base: 0x%lx\n",ptbr);
+    int index = 0xbc;
+    pageEntry* pt = (pageEntry*)ptbr;
+    pageEntry e = pt[0xbc];
+    e = 0x1234;
+    printf("chunk: 0x%llX\n",e);
     
+    return 0;
 }
