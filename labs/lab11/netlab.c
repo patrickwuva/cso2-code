22b89db64c1c62e528de3a5c305a6433a0544e0f
Patrick Williamson <upm8pb@virginia.edu>
2024-04-10 22:05:50 -0400

    updating

--- labs/lab11/netlab.c
+++ labs/lab11/netlab.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "netsim.h"
+
+int *seqs = NULL;
+int no_seqs = 0;
 
 int get_csum(char *data){
     int csum = 0;
@@ -9,10 +13,11 @@ int get_csum(char *data){
     return csum;
 }
 
-void ack(int last){
+void ack(void *last){
     char data[5];
-    data[1] = 'A'; data[2] = 'C'; data[3] = 'K'; data[4] = last;
+    data[1] = 'A'; data[2] = 'C'; data[3] = 'K'; data[4] = *(int *)last;
     data[0] = get_csum((char*)data);
+    free(last);
     send(5,data);
 }
 
@@ -20,29 +25,37 @@ void recvd(size_t len, void* _data) {
     char *data = _data;
     fwrite(data+3,1,len-3,stdout);
     fflush(stdout);
-    printf("[");
-    for(int i = 0; i < len; i++){
-        if (i < 3) printf("%d,",data[i]);
-    }
-    printf("]\n");
     int c_seq = data[1];
-    int l_seq = data[2];
-    if(c_seq < l_seq){
-        ack(c_seq);
+    if(no_seqs == 0){
+        no_seqs = data[2];   
+        seqs = (int *)calloc(no_seqs -1, sizeof(int));
+    }
+    if (c_seq == 1){
+        for (int i = 1; i < no_seqs - 1; i++){
+            int *c_seq_pt = malloc(sizeof(int));
+            *c_seq_pt = i;
+            seqs[c_seq - 1] = setTimeout(ack, 2500, (void *)c_seq_pt);
+        }      
+    }
+    else{
+        clearTimeout(seqs[c_seq - 1]);
     }
 }
 
 int main(int argc, char *argv[]) {
     // this code should work without modification
+
     if (argc != 2) {
         fprintf(stderr, "USAGE: %s n\n    where n is a number between 0 and 3\n", argv[0]);
         return 1;
     }
     char data[5];
     data[1] = 'G'; data[2] = 'E'; data[3] = 'T'; data[4] = argv[1][0];
-    
     data[0] = get_csum((char*)data);
     send(5, data);
-     
     waitForAllTimeoutsAndMessagesThenExit();
 }
+
+
+
+
