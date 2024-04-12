ba1d415423e71b310ce3ffa31d4d97a42688b9df
Patrick Williamson <upm8pb@virginia.edu>
2024-04-12 00:05:39 -0400

    updating

--- labs/lab11/netlab.c
+++ labs/lab11/netlab.c
@@ -2,54 +2,55 @@
 #include <stdlib.h>
 #include "netsim.h"
 
-typedef struct{
-    char *data;
-    size_t len;
-    int got_ack;
-    int tid;
-} packet;
-
-packet *packets = NULL;
-
-void init_packets(){
-    packets = calloc(6, sizeof(packet));
-}
-
-int get_csum(char *data){
-    int csum = 0;
-    for (int i = 1; i < 5; i++){
+char data[5];
+char input;
+int id, total, first, rec;
+int sqn = 1;
+void send_ack();
+int get_csum(){
+    int csum = data[1];
+    for (int i = 2; i < 5; i++){
         csum ^= data[i];
     }
     return csum;
 }
 
-void check_ack(void *arg){
-    int id = (int)(long)arg;
-    printf("pgot? %d\n", packets[id].got_ack);
-    if (!packets[id].got_ack){
-        send(packets[id].len, packets[id].data);
+void send_ack(){
+    if (input == '1' || input == '2'){
+        data[1] = 'A'; data[2] = 'C'; data[3] = 'K'; data[4] = id;
+        data[0] = get_csum();
+        send(5, data);
     }
-}
-
-void send_ack(int id){
-    char data[5];
-    data[1] = 'A'; data[2] = 'C'; data[3] = 'K'; data[4] = id;
-    data[0] = get_csum((char*)data);
-    packets[id].data = data;
-    packets[id].len = 5;
-    packets[id].got_ack = 0;
-    packets[id].tid = setTimeout(check_ack, 4000, (void*)(long)id);
-    send(5, data);
 }
 
 void recvd(size_t len, void* _data) {
     char *data = _data;
-    fwrite(data+3,1,len-3,stdout);
-    fflush(stdout);
-    int id = data[1];
-    send_ack(id);
-    packets[id].got_ack = 1;
-    clearTimeout(packets[id].tid);
+    
+    if(input == '0' || input == '1'){
+        fwrite(data+3,1,len-3,stdout);
+        id = (int)data[1];
+        total=(int)data[2];
+        data[4] = sqn;
+        fflush(stdout);
+        send_ack();
+    }
+    else if (input == '2') {
+        clearTimeout(first);
+        id = (int)data[1];
+        total = (int)data[2];
+        if (sqn == id){
+            fwrite(data+3,1,len-3,stdout);
+            data[4] = id;
+            fflush(stdout);
+            sqn++;
+            rec = id;
+            send_ack();
+        }
+        else if (id != sqn){
+            id = rec;
+            send_ack();
+        }
+    }
 }
 
 int main(int argc, char *argv[]) {
@@ -59,13 +60,24 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "USAGE: %s n\n    where n is a number between 0 and 3\n", argv[0]);
         return 1;
     }
-    init_packets();
-    
-    char data[5];
     data[1] = 'G'; data[2] = 'E'; data[3] = 'T'; data[4] = argv[1][0];
-    data[0] = get_csum((char*)data);
+    data[0] = get_csum();
+    input = data[4];
+    
     send(5, data);
     waitForAllTimeoutsAndMessagesThenExit();
+    if(input == '2'){
+        while(1){
+            if (sqn == total + 1){
+                clearTimeout(first);
+                break;
+            }
+            first = setTimeout(NULL, 1000, (void *)1UL);
+            send_ack();
+            waitForAllTimeoutsAndMessagesThenExit();
+        }
+           clearTimeout(first);
+    }
 }
 
 
